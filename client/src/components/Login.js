d3612b6b3b141bc5a0d9fc18870bee05013a3c59
ankitjena <ankitjena13@gmail.com>
2020-03-24 18:00:46 +0530

    feat: configure tailwindcss

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,12 +16,13 @@ const stringifiedParams = queryString.stringify({
 const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
 
 export function Login() {
-  console.log(process.env.REACT_APP_CLIENT_ID)
   return (
-    <div>
-      <button>
-        <a href={googleLoginUrl}>Login to google</a>
-      </button>
+    <div className="max-w-sm mx-auto flex my-12 p-6 bg-white rounded-lg shadow-xl">
+      <div className="mx-auto">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <a href={googleLoginUrl}> Login to google </a>
+        </button>
+      </div>
     </div>
   )
 }
