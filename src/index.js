14fad427aaf25f0184b6153c1f08912eea4debc6
ganeshpatro321 <ganeshpatro321@gmail.com>
2020-03-26 14:28:54 +0530

    graphql server setup and integration with mongodb
    Signed-off-by: ganeshpatro321 <ganeshpatro321@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
 import apiRouter from './routes/api'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+import { prepare } from './utils/index'
 
 const app = express()
 
@@ -25,6 +27,47 @@ mongoose.connect(
     }
   },
 )
+
+var db = mongoose.connection
+
+const Users = db.collection('users')
+
+const typeDefs = [
+  `
+  type Query {
+    user(_id: String): User
+    users: [User]
+  }
+
+  type User {
+    _id: String,
+    email: String,
+    name: String,
+    username: String
+  }
+`,
+]
+
+const resolvers = {
+  Query: {
+    user: async (root, { _id }) => {
+      return prepare(
+        await Users.findOne(mongoose.mongo.ObjectID(_id)),
+      )
+    },
+
+    users: async () => {
+      return (await Users.find({}).toArray()).map(prepare)
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.applyMiddleware({ app, path: '/graphql' })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
